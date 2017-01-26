80466b7adc506dc1b5e170f94d5560c18257c18b
Filip Jaros <fjaros91@gmail.com>
2017-01-25 20:31:40 -0800

    PinnedDispatcher test

--- src/main/scala/com/init6/users/UserActor.scala
+++ src/main/scala/com/init6/users/UserActor.scala
@@ -314,20 +314,20 @@ class UserActor(connection: ActorRef, var user: User, encoder: Encoder)
   }
 
   private def joinChannel(channel: String) = {
-    //implicit val timeout = Timeout(2, TimeUnit.SECONDS)
+    implicit val timeout = Timeout(2, TimeUnit.SECONDS)
     //println(user.name + " - " + self + " - SENDING JOIN")
-    channelsActor ! UserSwitchedChat(self, user, channel)
-//    Await.result(channelsActor ? UserSwitchedChat(self, user, channel), timeout.duration) match {
-//      case ChannelJoinResponse(event) =>
-//        //println(user.name + " - " + self + " - RECEIVED JOIN")
-//        event match {
-//          case UserChannel(newUser, channel, channelActor) =>
-//            user = newUser
-//            this.channelActor = channelActor
-//            channelActor ! GetUsers
-//          case _ =>
-//        }
-//        encodeAndSend(event)
-//    }
+//    channelsActor ! UserSwitchedChat(self, user, channel)
+    Await.result(channelsActor ? UserSwitchedChat(self, user, channel), timeout.duration) match {
+      case ChannelJoinResponse(event) =>
+        //println(user.name + " - " + self + " - RECEIVED JOIN")
+        event match {
+          case UserChannel(newUser, channel, channelActor) =>
+            user = newUser
+            this.channelActor = channelActor
+            channelActor ! GetUsers
+          case _ =>
+        }
+        encodeAndSend(event)
+    }
   }
 }
