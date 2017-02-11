4a8061cfb47768af4a59f36f652ba81e4b2b9d37
Filip Jaros <fjaros91@gmail.com>
2017-02-10 22:25:42 -0800

    akka 2.4.17

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@
 mvn clean package
 
 scp -P22022 target/init6.jar vilenet@dal.wserv.org:/home/vilenet/init6-test
+scp -P22022 src/main/resources/akka.conf vilenet@dal.wserv.org:/home/vilenet/init6-test
 ssh -p22022 -f vilenet@dal.wserv.org "./kill_test.sh"
 ssh -p22022 -f vilenet@dal.wserv.org "cd init6-test; ./start.sh --debug true --test"
 
 scp -P22022 target/init6.jar vilenet@sea.wserv.org:/home/vilenet/init6-test
+scp -P22022 src/main/resources/akka.conf vilenet@sea.wserv.org:/home/vilenet/init6-test
 ssh -p22022 -f vilenet@sea.wserv.org "./kill_test.sh"
 ssh -p22022 -f vilenet@sea.wserv.org "cd init6-test; ./start.sh --debug true --test"
 
