df3a96b45202ecf1d938fc1ef432b8622b60edb2
Filip Jaros <fjaros91@gmail.com>
2017-02-16 23:39:44 -0800

    check server cron

---
+++ src/main/resources/check_server_cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $(ps x|grep 'start.sh'|grep -v grep|wc -l) -ne 1 ]; then
+    # just in case
+    killall -9 start.sh java
+    sleep 10
+
+    cd /home/i6serverhost/init6
+    ./start.sh &> /dev/null &
+fi
